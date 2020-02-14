502504e96b62af8e4a6e7be83615396b1f6ee68f
Ewoud Werkman <ewoud.werkman@tno.nl>
2020-02-14 11:34:21 +0100

    Update esdl/esdl.ecore to latest version to get better documentation

--- updateESDL.sh
+++ updateESDL.sh
@@ -1,1 +1,2 @@
 pyecoregen -e https://raw.githubusercontent.com/EnergyTransition/ESDL/master/esdl/model/esdl.ecore -o . --auto-register-package --verbose
+wget https://raw.githubusercontent.com/EnergyTransition/ESDL/master/esdl/model/esdl.ecore -O esdl/esdl.ecore
