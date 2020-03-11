44668ab1f1bc145a3afca714bb61610ce79265c1
Ewoud Werkman <ewoud.werkman@tno.nl>
2020-03-11 14:30:23 +0100

    Fix defuxedxml dependency not necessary in newer pyEcore (uses lxml)

--- esdl/resources/xmlresource.py
+++ esdl/resources/xmlresource.py
@@ -1,7 +1,11 @@
-from pyecore.resources.xmi import XMIResource, XMIOptions, XMI_URL, XSI_URL,\
-    XSI
-from defusedxml.lxml import _etree as etree
+from pyecore.resources.xmi import XMIResource, XMIOptions, XMI_URL, XSI_URL, XSI
+from lxml.etree import QName, Element, ElementTree
 
+
+"""
+Extension of pyecore's XMIResource to support the XMLResource in EMF.
+It basically removes the xmi:version stuff from the serialization.
+"""
 class XMLResource(XMIResource):
     def __init__(self, uri=None, use_uuid=False):
         super().__init__(uri, use_uuid)
@@ -26,20 +30,20 @@ class XMLResource(XMIResource):
             tmp_xmi_root = self._go_across(root, serialize_default)
         else:
             # this case hasn't been verified for XML serialization
-            tag = etree.QName(XMI_URL, 'XMI')
-            tmp_xmi_root = etree.Element(tag)
+            tag = QName(XMI_URL, 'XMI')
+            tmp_xmi_root = Element(tag)
             for root in self.contents:
                 root_node = self._go_across(root, serialize_default)
                 tmp_xmi_root.append(root_node)
 
         # update nsmap with prefixes register during the nodes creation
         nsmap.update(self.prefixes)
-        xmi_root = etree.Element(tmp_xmi_root.tag, nsmap=nsmap)
+        xmi_root = Element(tmp_xmi_root.tag, nsmap=nsmap)
         xmi_root[:] = tmp_xmi_root[:]
         xmi_root.attrib.update(tmp_xmi_root.attrib)
         #xmi_version = etree.QName(XMI_URL, 'version') # remove XMI version in XML serialization
         #xmi_root.attrib[xmi_version] = '2.0'
-        tree = etree.ElementTree(xmi_root)
+        tree = ElementTree(xmi_root)
         tree.write(output,
                    pretty_print=True,
                    xml_declaration=True,
