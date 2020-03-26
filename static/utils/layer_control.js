90407d0fe070a82492ec24c1764fd1bc06e693e3
edwinmatthijssen <edwin.matthijssen@tno.nl>
2020-03-27 00:05:19 +0100

    Fixed last issues with jstree layer control; TODO: add behavior

--- static/utils/layer_control.js
+++ static/utils/layer_control.js
@@ -68,42 +68,49 @@ function add_layer_control() {
                         "icon" : "fas fa-layer-group layer-node"
                     }
                 }
-            });
-
+            })
+            .jstree(true).load_node('#');   // to get the checkbox into the selected state (initially)
 
         $esdl_layer_control_tree = $('<div>').attr('id', 'esdl_lct');
         $(div).append($esdl_layer_control_tree);
 
         $esdl_layer_control_tree
-            .on('select_node.jstree', function(e, data) {
-                console.log('select_node event');
-                console.log(data);
+//            .on('select_node.jstree', function(e, data) {
+//                console.log('select_node event');
+//                console.log(data);
+//            })
+//            .on('check_node.jstree', function(e, data) {
+//                console.log('check_node event');
+//                console.log(data);
+//            })
+//            .on('uncheck_node.jstree', function(e, data) {
+//                console.log('uncheck_node event');
+//                console.log(data);
+//            })
+//            .on("dblclick.jstree", function (e) {
+//                var instance = $.jstree.reference(this),
+//                node = instance.get_node(e.target);
+//                console.log('Double click event');
+//            })
+            .on('click', '.jstree-anchor', function (e, data) {
+//                console.log('click event on .jstree-anchor');
+//                console.log($('#esdl_lct').jstree(true).is_checked(e.target));
+//                console.log($(e.target.parentNode).hasClass('jstree-checked'))
+//                if ($(e.target.parentNode).hasClass('jstree-checked'))
+
+                // only has this behaviour on the sublayer level...   else check-uncheck again
+                if ($(e.target.parentNode.parentNode).attr('aria-level') == 3) {
+                    // this enables that esdl sublayers can be checked and unchecked while not being selected
+                    if ($('#esdl_lct').jstree(true).is_checked(e.target))
+                        $('#esdl_lct').jstree(true).uncheck_node(e.target);
+                    else
+                        $('#esdl_lct').jstree(true).check_node(e.target);
+                }
             })
-            .on('check_node.jstree', function(e, data) {
-                console.log('check_node event');
-                console.log(data);
-            })
-            .on('uncheck_node.jstree', function(e, data) {
-                console.log('uncheck_node event');
-                console.log(data);
-            })
-            .on("dblclick.jstree", function (e) {
-                var instance = $.jstree.reference(this),
-                node = instance.get_node(e.target);
-                console.log('Double click event');
-            })
-            .on('click', '.jstree-anchor', function (e) {
-                console.log('click event on .jstree-anchor');
-                // this enables that esdl sublayers can be checked and unchecked while not being selected
-                if ($('#esdl_lct').jstree(true).is_checked(e.target))
-                    $('#esdl_lct').jstree(true).uncheck_node(e.target);
-                else
-                    $('#esdl_lct').jstree(true).check_node(e.target);
-            })
-            .on('deselect_node.jstree', function(e, data) {
-                console.log('deselect_node event');
-                console.log(data);
-            })
+//            .on('deselect_node.jstree', function(e, data) {
+//                console.log('deselect_node event');
+//                console.log(data);
+//            })
             .jstree({
                 "core" : {
                     "data": get_esdl_layer_control_tree_data(),
@@ -125,13 +132,15 @@ function add_layer_control() {
                 "checkbox": {
                     "three_state": false,
                     "whole_node": false,
-                    "tie_selection": false
+                    "tie_selection": false      // uncouple selecting and checking
                 },
                 "conditionalselect" : function (node, e) {
-                    console.log(node);
+//                    console.log(node);
                     if(node.type === 'esdl-file') {
                         console.log('Selected an ESDL file!!!');
-                        $('#esdl_lct').jstree(true).check_node(e.target);
+                        // The next line checks the checkbox when clicking the title, but prevent the checkbox
+                        // from being clocked itself --> results in check-uncheck
+//                        $('#esdl_lct').jstree(true).check_node(e.target);
                         return true;
                     } else
                         return false;
@@ -164,12 +173,16 @@ function get_base_layers_control_tree_data() {
     let children = [];
 
     for (let i=0; i<baseTree['children'].length; i++) {
+        let state = {};
+        if (i==0)
+            state = { checked: true };
+
         let child = {
             id : baseTree['children'][i].label,
             text: baseTree['children'][i].label,
             icon: "",
             type: "base-layer",
-            state: {},
+            state: state,
             children: [],
             li_attr: {},
             a_attr: {}
