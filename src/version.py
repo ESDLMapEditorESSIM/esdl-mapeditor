f9837fad089f9a6bac42d6aa74bea51f65534e07
Ewoud Werkman <ewoud.werkman@tno.nl>
2021-04-29 10:45:57 +0200

    21.4.3

--- src/version.py
+++ src/version.py
@@ -1,3 +1,3 @@
-__version__ = "21.4.2"
-__long_version__ = "21.4.2-1-g9d1bf57"
-__git_commit__ = "9d1bf5756b919e718db2a4981e2b0813caa60f52"
+__version__ = "21.4.3"
+__long_version__ = "21.4.3-2-gb286991"
+__git_commit__ = "b2869917a6a05225e82c42923e9c71ae477f764b"
