923f88b1868d0a58260819654461baba083d5de5
Ewoud Werkman <ewoud.werkman@tno.nl>
2021-04-29 10:57:42 +0200

    Update version script

--- update_version.sh
+++ update_version.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 V=`npm run env | grep npm_package_version | cut -f 2 -d=`
 # number of commits since last tag
-N=`git rev-list $(git describe --abbrev=0)..HEAD --count`
+#N=`git rev-list $(git describe --abbrev=0)..HEAD --count`
+#branch
+B=`git rev-parse --abbrev-ref HEAD`
 #short git commit description
 S=`git rev-parse --short HEAD`
 #H=`git describe --always --long`
 # compose a version compatible with git describe, but with newer version
-H="$V-$N-g$S"
+H="$V-g$S-$B"
 C=`git rev-parse --verify HEAD`
 echo $H
 VERSION_PY=$(cat <<EOF
 __version__ = "$V"
 __long_version__ = "$H"
 __git_commit__ = "$C"
+__git_branch__ = "$B"
 EOF
 )
 echo "$VERSION_PY" > src/version.py
