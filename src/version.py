b2869917a6a05225e82c42923e9c71ae477f764b
Ewoud Werkman <ewoud.werkman@tno.nl>
2021-04-29 10:45:02 +0200

    Update version script

--- src/version.py
+++ src/version.py
@@ -1,2 +1,3 @@
-__version__ = "v21.4.2"
-__long_version__ = "v21.4.2-0-g3843c95"
+__version__ = "21.4.2"
+__long_version__ = "21.4.2-1-g9d1bf57"
+__git_commit__ = "9d1bf5756b919e718db2a4981e2b0813caa60f52"
--- update_version.sh
+++ update_version.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
-V=`git describe`
-H=`git describe --always --long`
+V=`npm run env | grep npm_package_version | cut -f 2 -d=`
+# number of commits since last tag
+N=`git rev-list $(git describe --abbrev=0)..HEAD --count`
+#short git commit description
+S=`git rev-parse --short HEAD`
+#H=`git describe --always --long`
+# compose a version compatible with git describe, but with newer version
+H="$V-$N-g$S"
+C=`git rev-parse --verify HEAD`
 echo $H
 VERSION_PY=$(cat <<EOF
 __version__ = "$V"
 __long_version__ = "$H"
+__git_commit__ = "$C"
 EOF
 )
 echo "$VERSION_PY" > src/version.py
