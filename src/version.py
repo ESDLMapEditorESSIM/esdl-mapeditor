f9a1de9c000824c867304b458df54bb3b5869d53
Edwin Matthijssen <edwin.matthijssen@tno.nl>
2021-05-03 22:52:15 +0200

    21.5.0

--- src/version.py
+++ src/version.py
@@ -1,4 +1,4 @@
-__version__ = "21.4.5"
-__long_version__ = "21.4.5-gc8e0b28-main"
-__git_commit__ = "c8e0b28b9bb32f1f8e77da1355a32939b604cd99"
+__version__ = "21.5.0"
+__long_version__ = "21.5.0-g7b61675-main"
+__git_commit__ = "7b6167504e5d4df96e766b198cdab9f3a0703a15"
 __git_branch__ = "main"
