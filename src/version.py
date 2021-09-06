10be8f638992528b96be11f569418496b13b4dac
Edwin Matthijssen <edwin.matthijssen@tno.nl>
2021-09-06 22:17:27 +0200

    21.9.0

--- src/version.py
+++ src/version.py
@@ -1,4 +1,4 @@
-__version__ = "21.8.0"
-__long_version__ = "21.8.0-g45d1bcd-main"
-__git_commit__ = "45d1bcd2a187aef3f5e074714a34f59d5918be27"
+__version__ = "21.9.0"
+__long_version__ = "21.9.0-g9fb35a9-main"
+__git_commit__ = "9fb35a9e3e852ad0a285548f3dd0ed3cb3cd00cf"
 __git_branch__ = "main"
