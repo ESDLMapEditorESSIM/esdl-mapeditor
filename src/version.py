3e0ee7b7888c0008a263727e140d5bc00ee1422e
Edwin Matthijssen <edwin.matthijssen@tno.nl>
2022-04-13 20:20:12 +0200

    22.3.0

--- src/version.py
+++ src/version.py
@@ -1,4 +1,4 @@
-__version__ = "22.2.0"
-__long_version__ = "22.2.0-g28bd138-main"
-__git_commit__ = "28bd138a41febb3838e6729a1c07761b8491c3d4"
+__version__ = "22.3.0"
+__long_version__ = "22.3.0-ge1c8b17-main"
+__git_commit__ = "e1c8b173622287ed031dba9671f49517d43e0ffc"
 __git_branch__ = "main"
