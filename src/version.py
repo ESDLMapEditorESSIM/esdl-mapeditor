5fff1f368e1643394f13ea50c82aa83ca71bc7e0
Edwin Matthijssen <edwin.matthijssen@tno.nl>
2022-10-31 22:14:19 +0100

    Updated MapEditor version

--- src/version.py
+++ src/version.py
@@ -1,4 +1,4 @@
-__version__ = "22.7.0"
-__long_version__ = "22.7.0-g778faf5-main"
-__git_commit__ = "778faf5084581c89441624f00d10ef2cb68b07a0"
+__version__ = "22.10.0"
+__long_version__ = "22.10.0-g0e52320-main"
+__git_commit__ = "0e523209dc290e19aea83d16851e7856f6aaf52b"
 __git_branch__ = "main"
