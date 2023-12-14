9e2b5d55ae38353db7e44636797be69d9c1bf120
Ewoud Werkman <ewoud.werkman@tno.nl>
2023-12-14 15:30:57 +0100

    Let Gitlab CI build image with yarn/vue frontend too

--- src/version.py
+++ src/version.py
@@ -1,4 +1,4 @@
 __version__ = "23.12.0"
-__long_version__ = "23.12.0-gad4a437-nwn-poc"
-__git_commit__ = "ad4a437f847e0355e4b2cfd82d9cf18e476f1b77"
+__long_version__ = "23.12.0-ga65a331-nwn-poc"
+__git_commit__ = "a65a331028da6470ea1e5b29957f13e03914c718"
 __git_branch__ = "nwn-poc"
